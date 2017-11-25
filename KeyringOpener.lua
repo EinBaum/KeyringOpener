e740fbf8298df1d539ff6d5ea76331c7da778ea4
EinBaum <mail@einbaum.org>
2017-11-25 15:02:28 +0100

    Initial commit

---
+++ KeyringOpener.lua
@@ -0,0 +1,17 @@
+local o = OpenAllBags
+OpenAllBags = function(forceOpen)
+	local b = ToggleBag
+	local s = PlaySound
+
+	PlaySound = function() end
+	ToggleBag = function(num)
+		b(num)
+		if num == 4 then
+			ToggleKeyRing()
+		end
+	end
+	o()
+
+	PlaySound = s
+	ToggleBag = b
+end
---
+++ README.md
@@ -0,0 +1,4 @@
+# KeyringOpener
+
+Opens the keyring whenever all bags are opened (Shift-B).
+It also mutes the bag sounds.
